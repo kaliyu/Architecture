4f61ce29d648ccb5b2cef2044472b7b2d1512a0a
o22100171 <tom.roy@sfr.fr>
2025-09-10 16:44:04 +0200

    exercice 3

--- TD1/C_exercice3/src/main/java/servlets/Exo3Servlet.java
+++ TD1/C_exercice3/src/main/java/servlets/Exo3Servlet.java
@@ -12,22 +12,54 @@ import java.io.IOException;
 public class Exo3Servlet extends HttpServlet {
     private String aDeviner=null;
     private StringBuilder devine=null;
-    private int nbEssaisRestants;
+    private int nbEssaisRestants = 10;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO ce qui est fait au premier appel
+        doWhatever(request,response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO ce qui est fait pour les appels suivants...
+        doWhatever(request,response);
+    }
+
+    private void doWhatever(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String todo=request.getParameter("TODO");
+
+        if(todo==null){
+            request.getRequestDispatcher("/WEB-INF/pendu.jsp").forward(request,response);
+        } else if (nbEssaisRestants <=1) {
+
+            request.setAttribute("perdu", "Vous avez perdu !");
+            request.getRequestDispatcher("/WEB-INF/pendu.jsp").forward(request,response);
+        }else{
+            switch (todo){
+                case "mot":
+                    setaDeviner(request.getParameter("lemot"));
+                    request.setAttribute("nbessai", nbEssaisRestants);
+                    request.setAttribute("devine", devine);
+                    request.getRequestDispatcher("/WEB-INF/essai.jsp").forward(request,response);
+                    break;
+                case "char":
+                    char caractere = request.getParameter("lecaractere").charAt(0);
+                    test(caractere);
+                    request.setAttribute("nbessai", nbEssaisRestants);
+                    request.setAttribute("devine", devine);
+                    if(devine.toString().equals(aDeviner)){
+                        request.setAttribute("perdu", "Vous avez Gagné !");
+                        request.getRequestDispatcher("/WEB-INF/pendu.jsp").forward(request,response);
+                    }else {
+                        request.getRequestDispatcher("/WEB-INF/essai.jsp").forward(request,response);
+                    }
+
+            }
+        }
     }
 
 
     private void setaDeviner(String Deviner) {
-        this.aDeviner=aDeviner;
+        this.aDeviner=Deviner;
         this.devine=new StringBuilder("_".repeat(aDeviner.length()));
-        this.nbEssaisRestants=10;
     }
 
     private boolean test(char carac){
@@ -38,7 +70,7 @@ public class Exo3Servlet extends HttpServlet {
             devine.setCharAt(last, carac);
             last=aDeviner.indexOf(carac,last+1);
         }
-        if (res==false) {
+        if (!res) {
             nbEssaisRestants--;
         }
         return aDeviner.equals(devine.toString());
